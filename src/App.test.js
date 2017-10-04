fc6996ef46e9575d4ebe0812c0d5f72ae2a72ae3
Dominique Busser <dominique.busser@netlight.com>
2017-10-04 11:02:10 +0200

    Breaking tests for border-color and background-color

--- src/App.test.js
+++ src/App.test.js
@@ -6,29 +6,63 @@ const myTheme = {
   colors: {
     one: "#fff",
     two: "#0f0",
-    three: "#f0f"
+    three: "#f0f",
+    four: "#00f"
   },
   dimensions: {
+    small: "1px",
     large: "10px"
   }
 };
 
 const Parent = styled.div`
-  border-color: ${props => props.one ? props.theme.colors.one : props.theme.colors.two};
+  border-color: ${props =>
+    props.one ? props.theme.colors.one : props.theme.colors.two};
+  background-color: ${props => props.theme.colors.three };
+  width: ${props => props.theme.dimensions.small };
 `;
 
-const Child = styled(Parent)``;
+const Child = styled(Parent)`
+  width: ${props => props.theme.dimensions.large };
+`;
 
-test("Parent", () => {
-  const wrapper = mount(
-    <ThemeProvider theme={myTheme}>
-      <Parent>
-        <Child />
-      </Parent>
-    </ThemeProvider>
-  );
-  expect(wrapper.find(Parent)).toHaveStyleRule(
-    "border-color",
-    myTheme.colors.two
-  );
+describe("Theme is applied correctly", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <ThemeProvider theme={myTheme}>
+        <Parent one>
+          <Child />
+        </Parent>
+      </ThemeProvider>
+    );
+  });
+
+  test("Parent has correct border color", () => {
+    expect(wrapper.find(Parent)).toHaveStyleRule(
+      "border-color",
+      myTheme.colors.one
+    );
+  });
+
+  // This test breaks, border-color property not found
+  test("Child has correct inherited border color", () => {
+    expect(wrapper.find(Child)).toHaveStyleRule("border-color", myTheme.colors.one);
+  })
+
+  // This test breaks, background-color property not found 
+  test("Child has correct background color", () => {
+    expect(wrapper.find(Child)).toHaveStyleRule("background-color", myTheme.colors.four);
+  })
+
+  test("Parent has correct width", () => {
+    expect(wrapper.find(Parent)).toHaveStyleRule("width", myTheme.dimensions.small);
+  })
+
+  // Child overrides width from Parent
+  test("Child has correct width", () => {
+    expect(wrapper.find(Child)).toHaveStyleRule("width", myTheme.dimensions.large);
+  })
+
 });
