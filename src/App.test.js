433631a23d4dec4cbd4628fa4458b3ea3492426d
Dominique Busser <dominique.busser@netlight.com>
2017-12-01 13:26:11 +0100

    Sample tests for :disabled style rules

--- src/App.test.js
+++ src/App.test.js
@@ -1,55 +1,31 @@
-import React from "react";
-import { mount } from "enzyme";
-import styled, { ThemeProvider } from "styled-components";
+import React from 'react';
+import { shallow } from 'enzyme';
+import styled from 'styled-components';
 
-const myTheme = {
-  colors: {
-    one: "#fff",
-    two: "#0f0",
-    three: "#f0f",
-    four: "#00f"
-  }
+const colors = {
+  readonly: 'lightgray'
 };
 
-const Parent = styled.div`
-  border-color: ${props =>
-    props.one ? props.theme.colors.one : props.theme.colors.two};
-  background-color: ${props => props.theme.colors.three };
+const readonlyStyles = `
+border-radius: 3px;
+
+&:disabled,
+&:read-only {
+  background-color: ${colors.readonly};
+}
 `;
 
-// Change Child to Parent.extend`` to make tests pass
-const Child = styled(Parent)`
-  background-color: ${props => props.theme.colors.four };
+const InputField = styled.input`
+  ${readonlyStyles};
+  height: 20px;
 `;
 
-describe("Theme is applied correctly", () => {
-  let wrapper;
+test('renders correctly with readOnly prop', () => {
+  const wrapper = shallow(<InputField readOnly />);
+  expect(wrapper).toHaveStyleRule('background-color', colors.readonly);
+});
 
-  beforeEach(() => {
-    wrapper = mount(
-      <ThemeProvider theme={myTheme}>
-        <div>
-          <Parent one />
-          <Child one/>
-        </div>
-      </ThemeProvider>
-    );
-  });
-
-  test("Parent has the correct border-color", () => {
-    expect(wrapper.find(Parent)).toHaveStyleRule(
-      "border-color",
-      myTheme.colors.one
-    );
-  });
-
-  test("Child has the correct background-color", () => {
-    expect(wrapper.find(Child)).toHaveStyleRule("background-color", myTheme.colors.four);
-  })
-
-  // This test breaks, border-color property not found in Child
-  test("Child has the correct inherited border-color", () => {
-    expect(wrapper.find(Child)).toHaveStyleRule("border-color", myTheme.colors.one);
-  })
-
+test('renders correct border', () => {
+  const wrapper = shallow(<InputField readOnly />);
+  expect(wrapper).toHaveStyleRule('border-radius', '3px');
 });
